6caae605d44928bcf53a6ed01c2449929cae1c91
Preethi <writetopreethir@gmail.com>
2022-11-03 21:12:10 -0700

    Weather Service Modified

--- src/app/show.service.ts
+++ src/app/show.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShowService {
 
-  constructor() { 
+  constructor(private httpClient : HttpClient) { 
       
     
   }
