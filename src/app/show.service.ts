964cd1531e090ea03cab86d463f07968ed4eb7e3
Preethi <writetopreethir@gmail.com>
2022-11-05 11:10:37 -0700

    Weather Service

--- src/app/show.service.ts
+++ src/app/show.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ITvshow } from './itvshow';
+import { IShowInfoData } from './ishow-info-data';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,23 @@ export class ShowService {
   }
 
     getShowData(val : any) {
-    
+    var result =  this.httpClient.get<IShowInfoData>(` https://api.tvmaze.com/singlesearch/shows?q=girls`);
+    console.log('Result:: ',result);
+    return result.pipe(map(data => this.transformtoShowData(data)));
   }
+  
+    private transformtoShowData(data:IShowInfoData) {
+    return {
+      id: data.id,
+      title: data.name,
+      language: data.language,
+      genre: data.genres,
+      rating: data.rating.average,
+      summary: data.summary,
+      status:data.status,
+      img: data.image? data.image.original:'',
+      ended: data.ended,
+    }
+     
+    }
 }
