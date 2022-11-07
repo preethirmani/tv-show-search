4649a4bfa9f7f13e48a636dd8d5c931431f1bf6a
maryl-in32 <115038241+maryl-in32@users.noreply.github.com>
2022-11-06 20:21:02 -0800

    component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowComponentComponent } from './show-component/show-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowComponentComponent
   ],
   imports: [
     BrowserModule,
